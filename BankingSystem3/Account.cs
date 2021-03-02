0190ce48d3bdca21c354ece256cde1442e8d10bb
Nahid-Hasan551 <nahidhasannibir121370@gmail.com>
2021-03-02 12:51:33 +0600

    first commit

---
+++ BankingSystem3/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem3
+{
+    class Account
+
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+        public static int lastnumber = 1001;
+
+        public Account(int accountNumber, string accountName, double balance, Address address)
+        {
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = balance;
+            this.address = address;
+        }
+        public int AccountNumber
+        {
+            set { this.accountNumber = value; }
+            get { return this.accountNumber; }
+        }
+
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+        public void Withdraw(double amount)
+        {
+            this.Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            this.Balance += amount;
+        }
+        public void Transfer(Account account, double amount)
+        {
+            account.Balance += amount; 
+        }
+
+        public void PrintAccount()
+        {
+            //Console.WriteLine("Account No:"+this.accountNumber+"\nAccount Name:"+this.accountName+"\nBalance:"+this.balance);
+            Console.WriteLine("Account No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            this.address.PrintAddress();
+        }
+    }
+}
---
+++ BankingSystem3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Address address1 = new Address(4,10,"Dhaka");
+            //Account account1 = new Account(1001, "Shakib", 2000, address1);
+            //account1.PrintAccount();
+
+            //Account account2 = new Account(1002,"Tamim",3000,new Address(7,20,"Chittagong"));
+            //account2.PrintAccount();
+
+            Bank ourBank = new Bank("Developer's bank", 5);
+            ourBank.AddAccount(new Account(8, "Shakib", 2000, new Address(4, 10, "Dhaka", "Bangladesh")));
+
+
+            //ourBank.PrintAllAccounts();
+
+            //   Bank ourBank = new Bank("Developer's bank", 5);
+            //  ourBank.AddAccount(new Account(1001, "Shakib", 2000, new Address(4, 10, "Dhaka")));
+
+            //ourBank.PrintAllAccounts();
+            //ourBank.DeleteAccount(8);
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine("1.Add");
+                Console.WriteLine("2.Delete");
+                Console.WriteLine("3.Show Account Informain");
+                Console.WriteLine("4.Print AllAccount");
+                int ch = int.Parse(Console.ReadLine());
+                if (ch == 1)
+                {
+                    Console.Write("Enter AC Name  To Create ");
+
+                    string name = (Console.ReadLine());
+                    Console.Write("Enter AC Balance  To Create ");
+
+                    int  balance = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter Roadnumber  To Create ");
+
+                    int road =int.Parse(Console.ReadLine());
+
+
+
+
+
+                    ourBank.AddAccount(new Account(Account.lastnumber++,name, balance, new Address(4, 10, "Dhaka", "Bangladesh")));
+                    Console.WriteLine("Account Create Successfuly for account number:"+(Account.lastnumber-1));
+
+                    continue;
+                   
+
+                }
+
+                else if (ch == 2)
+                {
+                    Console.Write("Enter AC No For Show Account Inforamtion:  ");
+                    int del = int.Parse(Console.ReadLine());
+                    ourBank.DeleteAccount(del);
+                    continue;
+                }
+                else if (ch == 3)
+                {
+                    Console.Write("Enter AC No For Show Account Inforamtion:  ");
+                    int acn = int.Parse(Console.ReadLine());
+                    ourBank.SearchAccount(acn);
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
